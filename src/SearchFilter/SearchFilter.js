4dd2e53ae2347cdefcccc351ced8193e2226e5d9
JS-Jamie <jamie.jeesoo.shin@gmail.com>
2022-11-06 11:39:01 -0800

    Add search and sort functionalities

--- src/SearchFilter/SearchFilter.js
+++ src/SearchFilter/SearchFilter.js
@@ -1,29 +1,58 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Card, Input } from "semantic-ui-react";
 import { mockData } from "./mockData";
 
 const SearchFilter = () => {
   const [input, setInput] = useState("");
+  const [option, setOption] = useState("viewCount");
   const [filteredResults, setFilteredResults] = useState([]);
+
+  useEffect(() => {
+    setFilteredResults(mockData);
+  }, []);
+
+  useEffect(() => {
+    console.log("option inside 2nd useEffect", option);
+    handleArrayUpdate();
+  }, [option, filteredResults]);
 
   const searchItems = (text) => {
     setInput(text);
     if (input.length > 0) {
-      const filteredData = mockData.filter((item) => {
+      const filteredData = filteredResults.filter((item) => {
         return Object.values(item)
           .join("")
           .toLowerCase()
           .includes(input.toLowerCase());
       });
       setFilteredResults(filteredData);
-    } else {
-      setFilteredResults(mockData);
     }
-    console.log(filteredResults);
   };
 
   const handleChange = (e) => {
     searchItems(e.target.value);
+  };
+
+  const handleSelectionChange = (e) => {
+    setOption(e.target.value);
+  };
+
+  const handleArrayUpdate = () => {
+    if (option) {
+      if (option === "viewCount") {
+        console.log("viewCount selected", filteredResults, option);
+        const sortedByViewCount = mockData.sort(
+          (a, b) => b.viewCount - a.viewCount
+        );
+        setFilteredResults(sortedByViewCount);
+      } else if (option === "newest") {
+        console.log("newest selected", filteredResults, option);
+        const sortedByNewest = mockData.sort(
+          (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
+        );
+        setFilteredResults(sortedByNewest);
+      }
+    }
   };
 
   return (
@@ -35,37 +64,33 @@ const SearchFilter = () => {
         onChange={handleChange}
         style={{ margin: "10px" }}
       />
+      <span>
+        <select
+          // value={option}
+          // defaultValue="viewCount"
+          onChange={handleSelectionChange}
+        >
+          <option value="viewCount">Most Viewed</option>
+          <option value="newest">Newest</option>
+        </select>
+      </span>
 
       <Card.Group>
-        {input.length > 0
-          ? filteredResults.map((item, id) => {
-              return (
-                <Card key={id}>
-                  <Card.Content>
-                    <Card.Header>{item.title}</Card.Header>
-                    <Card.Description>
-                      {item.author.firstName} {item.author.lastName}
-                    </Card.Description>
-                  </Card.Content>
-                  <Card.Content extra>{item.viewCount}</Card.Content>
-                </Card>
-              );
-            })
-          : mockData.map((item, id) => {
-              return (
-                <Card key={id}>
-                  <Card.Content>
-                    <Card.Header>{item.title}</Card.Header>
-                    <Card.Description>
-                      {item.author.firstName} {item.author.lastName}
-                    </Card.Description>
-                  </Card.Content>
-                  <Card.Content extra>
-                    View Count: {item.viewCount}
-                  </Card.Content>
-                </Card>
-              );
-            })}
+        {filteredResults &&
+          filteredResults.length > 0 &&
+          filteredResults.map((item, id) => {
+            return (
+              <Card key={id}>
+                <Card.Content>
+                  <Card.Header>{item.title}</Card.Header>
+                  <Card.Description>
+                    {item.author.firstName} {item.author.lastName}
+                  </Card.Description>
+                </Card.Content>
+                <Card.Content extra>{item.viewCount}</Card.Content>
+              </Card>
+            );
+          })}
       </Card.Group>
     </div>
   );
